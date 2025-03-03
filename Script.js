a0c69bedcde5830f15ea008492daa3f9139efa09
Milton Santos <163319280+koobotenMil@users.noreply.github.com>
2025-03-03 16:30:54 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -5,20 +5,20 @@ function gerarCodigo(button) {
     
     const promoCodeElement = button.closest('.card-body').querySelector('.promo-code');
     
- 
+    
     promoCodeElement.innerText = codigo;
 }
 
 const cards = document.querySelectorAll('.card');
 
 cards.forEach(card => {
-  
+    
     card.addEventListener('mouseenter', () => {
         card.style.transform = 'scale(1.05)';
         card.style.boxShadow = '0px 8px 16px rgba(0, 0, 0, 0.2)';
     });
 
-  
+    
     card.addEventListener('mouseleave', () => {
         card.style.transform = 'scale(1)';
         card.style.boxShadow = 'none';
